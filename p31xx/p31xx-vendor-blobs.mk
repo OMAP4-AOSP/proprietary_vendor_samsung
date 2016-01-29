2ebf4d351de94e7b3b3d22f5c18f7f14a6de11ce
Ziyan <jaraidaniel@gmail.com>
2016-01-29 12:27:59 +0100

    espresso: commonize wifi firmwares

--- p31xx/p31xx-vendor-blobs.mk
+++ p31xx/p31xx-vendor-blobs.mk
@@ -4,9 +4,3 @@ VENDOR_FOLDER := vendor/samsung/p31xx
 PRODUCT_COPY_FILES += \
     $(VENDOR_FOLDER)/proprietary/sbin/cbd:root/sbin/cbd \
     $(VENDOR_FOLDER)/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so
-
-# Wi-Fi
-PRODUCT_COPY_FILES += \
-    $(VENDOR_FOLDER)/proprietary/etc/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
-    $(VENDOR_FOLDER)/proprietary/etc/wifi/bcmdhd_mfg.bin:system/etc/wifi/bcmdhd_mfg.bin \
-    $(VENDOR_FOLDER)/proprietary/etc/wifi/bcmdhd_sta.bin:system/etc/wifi/bcmdhd_sta.bin
