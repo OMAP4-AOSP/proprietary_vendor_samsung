b2d4cd59222f3c144e4941d8259023928436049c
Ziyan <jaraidaniel@gmail.com>
2016-02-21 16:38:25 +0100

    Create common espresso3g vendor tree
    Using the p5100 libsec-ril, which seems to work fine on p3100 as well.

--- p31xx/p31xx-vendor-blobs.mk
+++
@@ -1,6 +0,0 @@
-VENDOR_FOLDER := vendor/samsung/p31xx
-
-# RIL
-PRODUCT_COPY_FILES += \
-    $(VENDOR_FOLDER)/proprietary/sbin/cbd:root/sbin/cbd \
-    $(VENDOR_FOLDER)/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so
