3fd627c54934bba43fea2a7aa8db567b72693323
Ziyan <jaraidaniel@gmail.com>
2016-03-24 20:26:42 +0100

    espresso: remove DRM blobs

--- espresso-common/common-vendor-blobs.mk
+++ espresso-common/common-vendor-blobs.mk
@@ -41,12 +41,3 @@ PRODUCT_COPY_FILES += \
 # Ducati
 PRODUCT_COPY_FILES += \
     $(VENDOR_FOLDER)/proprietary/vendor/firmware/ducati-m3.bin:system/vendor/firmware/ducati-m3.bin
-
-# DRM
-PRODUCT_COPY_FILES += \
-    $(VENDOR_FOLDER)/proprietary/system/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
-    $(VENDOR_FOLDER)/proprietary/system/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-    $(VENDOR_FOLDER)/proprietary/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-    $(VENDOR_FOLDER)/proprietary/system/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
-    $(VENDOR_FOLDER)/proprietary/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-    $(VENDOR_FOLDER)/proprietary/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
