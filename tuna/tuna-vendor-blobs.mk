fc029e28d2ff4c400ed1e6f1a87c59c4d0705174
Ziyan <jaraidaniel@gmail.com>
2016-03-24 20:22:15 +0100

    tuna: remove DRM blobs

--- tuna/tuna-vendor-blobs.mk
+++ tuna/tuna-vendor-blobs.mk
@@ -4,14 +4,8 @@ VENDOR_FOLDER := vendor/samsung/tuna/proprietary/vendor
 # common tuna files copied to default locations
 PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/firmware/ducati-m3.bin:system/vendor/firmware/ducati-m3.bin \
-	$(VENDOR_FOLDER)/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-	$(VENDOR_FOLDER)/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
-	$(VENDOR_FOLDER)/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
 	$(VENDOR_FOLDER)/lib/libfrsdk.so:system/vendor/lib/libfrsdk.so \
-	$(VENDOR_FOLDER)/lib/libinvensense_mpl.so:system/vendor/lib/libinvensense_mpl.so \
-	$(VENDOR_FOLDER)/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-	$(VENDOR_FOLDER)/lib/libwvm.so:system/vendor/lib/libwvm.so \
-	$(VENDOR_FOLDER)/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
+	$(VENDOR_FOLDER)/lib/libinvensense_mpl.so:system/vendor/lib/libinvensense_mpl.so
 
 # maguro-specific files copied to /system/vendor/maguro
 PRODUCT_COPY_FILES += \
