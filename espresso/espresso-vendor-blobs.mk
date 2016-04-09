c8634b31407516f9807c37b02a332d7fbdbd4c1f
Ziyan <jaraidaniel@gmail.com>
2016-04-09 12:58:49 +0200

    Rename espresso-common to espresso

--- espresso/espresso-vendor-blobs.mk
+++ espresso/espresso-vendor-blobs.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VENDOR_FOLDER := vendor/samsung/espresso-common
+VENDOR_FOLDER := vendor/samsung/espresso
 
 # SMC
 PRODUCT_COPY_FILES += \
--- espresso/espresso-vendor.mk
+++ espresso/espresso-vendor.mk
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/samsung/espresso-common/common-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/espresso/espresso-vendor-blobs.mk)
